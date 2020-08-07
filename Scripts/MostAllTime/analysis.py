86d2bb15ca460d5cedef6731306960374a2da359
Ajay Benno <abenno@yext.com>
2020-08-06 23:40:53 -0400

    MostAllTime: update script for new file format
    This changeset updates the most all time script for the new file type.
    The files used to be in one file called message.json but now FB splits
    messages in buckets of 10k so files will be split across message.json,
    message_1.json, message_2.json etc
    
    TEST=Manual
    
    Tested locally by redownloading current facebook data and running
    script.

--- Scripts/MostAllTime/analysis.py
+++ Scripts/MostAllTime/analysis.py
@@ -3,6 +3,7 @@ import json
 from datetime import datetime
 from heapq import nlargest
 from tqdm import tqdm
+import glob
 
 def main():
 	path = "../../messages/inbox"
@@ -16,26 +17,40 @@ def main():
 
 	lens = []
 
+	nameToCountMap = {}
+
 	for s in tqdm(l[1:len(l)-1]):
 		files = os.listdir(path + "/" + s)
-		with open(path + "/" + s + '/message.json') as f:
-			data = json.load(f)
+		for filename in glob.glob(path + "/" + s + '/message*.json'):
+			with open(filename) as f:
+				data = json.load(f)
 
-			if len(data['participants']) > 2:
-				continue
+				if len(data['participants']) > 2:
+					continue
 
-			names = []
-			for p in data['participants']:
-				names.append(p['name'].encode("utf-8"))
-			# print(names)
+				names = []
+				for p in data['participants']:
+					names.append(p['name'].encode("utf-8"))
 
-			messages = data['messages']
+				messages = data['messages']
+				for name in names:
+					if name in nameToCountMap:
+						nameToCountMap[name] = nameToCountMap[name] + len(messages)
+					else:
+						nameToCountMap[name] = len(messages)
 
-			lens.append([len(messages), s])
-			# print(len(messages))
-			# for m in messages:
-			# 	time = m["timestamp_ms"]/1000
-			# 	print(datetime.utcfromtimestamp(time).strftime('%Y-%m-%d %H:%M:%S'))
+
+
+				# lens.append([len(messages), names])
+				# print(len(messages))
+				# for m in messages:
+				# 	time = m["timestamp_ms"]/1000
+				# 	print(datetime.utcfromtimestamp(time).strftime('%Y-%m-%d %H:%M:%S')
+	lens = []
+	for key in nameToCountMap.keys():
+		lens.append([nameToCountMap[key],key])
+
+	# print nameToCountMap
 
 
 	x = sorted(lens, key=lambda i: i[0], reverse=True)[:30]
