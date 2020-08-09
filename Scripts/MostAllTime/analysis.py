5f30e6c174001a99a043c5022761b187f53ed3cb
Connor Lin <connorzl@google.com>
2020-08-08 19:56:18 -0700

    Add support for multiple input paths, processing each group chat message individually, and flag for including group chats in analysis.

--- Scripts/MostAllTime/analysis.py
+++ Scripts/MostAllTime/analysis.py
@@ -1,57 +1,56 @@
-import os
+import argparse
+import glob
 import json
+import os
 from datetime import datetime
 from heapq import nlargest
 from tqdm import tqdm
-import glob
 
 def main():
-	path = "../../messages/inbox"
-        # path = "../../../facebookdata/messages/inbox"
-	l = os.listdir(path) # dir is your directory path
-	number_files = len(l)
-	print(number_files)
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--path', action='append', type=str, help='Relative path to a messages directory. Repeat this flag to analyze several messages directories together.')
+	parser.add_argument('--include_group_chats', dest='group_chats', action='store_true', help='Include group chats in analysis.')
+	parser.add_argument('--ignore_group_chats', dest='group_chats', action='store_false', help='Ignore group chats in analysis.')
+	parser.set_defaults(group_chats=True)
+	args = parser.parse_args()
 
-	l = sorted(l, key=str.lower)
-	# testL = l[1:10]
+	paths = args.path
+	for i in range(len(paths)):
+		paths[i] = os.path.join(paths[i], 'inbox')
 
+	all_l = []
+	for path in paths:
+		l = os.listdir(path)
+		print("Input path {p} contains {n} files.".format(p=path, n=len(l)))
+		all_l += l
+	all_l = sorted(all_l, key=str.lower)
+	
 	lens = []
-
 	nameToCountMap = {}
+	for s in tqdm(all_l[1:len(all_l)-1]):
+		for path in paths:
+			path = os.path.join(path, s)
+			try: 
+				files = os.listdir(path)
+			except:
+				continue
+			for filename in glob.glob(os.path.join(path, 'message*.json')):
+				with open(filename) as f:
+					data = json.load(f)
 
-	for s in tqdm(l[1:len(l)-1]):
-		files = os.listdir(path + "/" + s)
-		for filename in glob.glob(path + "/" + s + '/message*.json'):
-			with open(filename) as f:
-				data = json.load(f)
+					if not(args.group_chats) and len(data['participants']) > 2:
+						continue
 
-				if len(data['participants']) > 2:
-					continue
+					for message in data['messages']:
+						sender_name = message['sender_name'].encode("utf-8")
+						if sender_name in nameToCountMap:
+							nameToCountMap[sender_name] += 1
+						else:
+							nameToCountMap[sender_name] = 1
 
-				names = []
-				for p in data['participants']:
-					names.append(p['name'].encode("utf-8"))
-
-				messages = data['messages']
-				for name in names:
-					if name in nameToCountMap:
-						nameToCountMap[name] = nameToCountMap[name] + len(messages)
-					else:
-						nameToCountMap[name] = len(messages)
-
-
-
-				# lens.append([len(messages), names])
-				# print(len(messages))
-				# for m in messages:
-				# 	time = m["timestamp_ms"]/1000
-				# 	print(datetime.utcfromtimestamp(time).strftime('%Y-%m-%d %H:%M:%S')
 	lens = []
 	for key in nameToCountMap.keys():
 		lens.append([nameToCountMap[key],key])
-
-	# print nameToCountMap
-
 
 	x = sorted(lens, key=lambda i: i[0], reverse=True)[:30]
 
